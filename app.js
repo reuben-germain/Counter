f4da0f32d3554ab4ec712255040feb5091f83129
Reuben Germain <reuben.germain@soprasteria.com>
2023-12-06 13:59:17 +0000

    added an increase & decrease const query selector

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const element = '#app';
 
 //Caching or retrieving refences to the DOM
 const rootElement = document.querySelector('#app');
+const increaseButton = document.querySelector('#increase');
+const decreaseButton = document.querySelector('#decrease');
 
 //Setup
 //Functions
