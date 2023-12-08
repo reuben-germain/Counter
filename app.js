37ee285e6b0d246c21f7d5bc01a6330693877ae6
Reuben Germain <reuben.germain@soprasteria.com>
2023-12-08 15:03:17 +0000

    comment out some code

--- app.js
+++ app.js
@@ -29,30 +29,30 @@ const Counter = {
 };
 Counter.init();
 
-//Data & Variables
-let count = 0;
-const element = '#app';
+// //Data & Variables
+// let count = 0;
+// const element = '#app';
 
-//Caching or retrieving refences to the DOM
-const rootElement = document.querySelector('#app');
-const increaseButton = document.querySelector('#increase');
-const decreaseButton = document.querySelector('#decrease');
-const display = document.querySelector('#display');
-console.log(rootElement, increaseButton, decreaseButton, display);
-//Setup
-decreaseButton.addEventListener('click', dec);
-increaseButton.addEventListener('click', inc);
-render();
-//Functions
-function dec(){
-    count -= 1;
-    render();
-}
-function inc(){
-    count += 1;
-    render();
-}
+// //Caching or retrieving refences to the DOM
+// const rootElement = document.querySelector('#app');
+// const increaseButton = document.querySelector('#increase');
+// const decreaseButton = document.querySelector('#decrease');
+// const display = document.querySelector('#display');
+// console.log(rootElement, increaseButton, decreaseButton, display);
+// //Setup
+// decreaseButton.addEventListener('click', dec);
+// increaseButton.addEventListener('click', inc);
+// render();
+// //Functions
+// function dec(){
+//     count -= 1;
+//     render();
+// }
+// function inc(){
+//     count += 1;
+//     render();
+// }
 
-function render(){
-    display.textContent = count;
-}
+// function render(){
+//     display.textContent = count;
+// }
